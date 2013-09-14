f1aeec348177189b50de338acb08bd6626288255
Guo Yu <i@guoyu.me>
2013-09-14 16:10:45 +0800

    是选scp还是包装api呢？这是个问题

---
+++ index.js
@@ -0,0 +1,12 @@
+//                   __         
+//   _________  ____/ /__  _____
+//  / ___/ __ \/ __  / _ \/ ___/
+// / /__/ /_/ / /_/ /  __/ /    
+// \___/\____/\__,_/\___/_/     
+//                              
+// @brief: a sdk of Coder, the dreaming webOS running on Pi
+// @author: [turingou](http://guoyu.me)
+
+exports.coder = require('./lib/coder');
+exports.app = require('./lib/app');
+exports.cli = require('./lib/cli');
